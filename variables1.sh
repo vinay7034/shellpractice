c021dca006a4d92330d71f574f010edd3030fcea
vinay <v@gmail.com>
2026-02-17 17:22:19 +0530

    variables1commit

---
+++ variables1.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1:: welcome to alaska $PERSON2"
+echo "$PERSON2:: Thank you $PERSON1, finally not on zoom webinar"
+echo "$PERSON1:: so, we have a deal"
+echo "$PERSON2:: yes, if you join Ukrain and Rassia"
